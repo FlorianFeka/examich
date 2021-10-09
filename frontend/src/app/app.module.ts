8387131e9b7119f2c004e34c6a2a1b3449c1a240
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-10-09 11:28:07 +0200

    add routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiModule } from 'src/api';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, NoopAnimationsModule, HttpClientModule, ApiModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    NoopAnimationsModule,
+    HttpClientModule,
+    ApiModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
