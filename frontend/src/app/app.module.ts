35c264768945088bc0264fdb054ce24e7f3b00bb
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-10-17 16:35:08 +0200

    add login page skeleton

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ApiModule } from 'src/api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
+import { LoginComponent } from './login/login.component';
 import { MaterialModule } from './material.module';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
   imports: [
-    MaterialModule,
-    BrowserModule,
-    NoopAnimationsModule,
-    HttpClientModule,
-    ApiModule,
     AppRoutingModule,
+    MaterialModule,
+    ApiModule,
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  exports: [MatToolbarModule],
+  exports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
 })
 export class MaterialModule {}
