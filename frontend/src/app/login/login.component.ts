456fff13f4fbd32c99b4b3c3f68ee3cc6e1101d7
Florian Feka <47530143+FlorianFeka@users.noreply.github.com>
2021-10-18 11:53:20 +0200

    add error message upon invalid login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/api';
 
 @Component({
@@ -10,7 +11,11 @@ import { AuthService } from 'src/api';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(fb: FormBuilder, private auth: AuthService) {
+  constructor(
+    fb: FormBuilder,
+    private auth: AuthService,
+    private snackBar: MatSnackBar
+  ) {
     this.loginForm = fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -29,6 +34,14 @@ export class LoginComponent implements OnInit {
       password: this.loginForm.get('password')?.value,
       rememberLogin: true,
     });
-    result.subscribe((a) => console.log(a));
+    result.subscribe(
+      (a) => console.log(a),
+      (err) => {
+        if (err.status === 401)
+          this.snackBar.open('Wrong email or password.', 'Dismiss', {
+            duration: 5000,
+          });
+      }
+    );
   }
 }
