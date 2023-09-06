cab302158de8be2e03b63a4ef21b2d4aaedd7ad0
Florian Feka <florianfeka05@gmail.com>
2023-09-06 02:06:05 +0200

    update k6s to use new token and influxdb server

--- api_performance_test/ExamPdf.js
+++ api_performance_test/ExamPdf.js
@@ -1,21 +1,24 @@
 import http from "k6/http";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYzQ1NWU2NS03MDNjLTQ3MDMtOGFiNy0wMzM3ZGRjZGMyMDMiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMiIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6Im1heEBnbWFpbC5jb20iLCJleHAiOjE3MTYyMDM0OTEsImlzcyI6Ind3dy5leGFtaWNoLmlvIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMS8ifQ.Od9B6dHL9JbkzNYNOA0UOZc7cmjyZrq-O7EvD_ynLJU";
+import getTestUserAuthToken from './pre/Auth'
+import { monoURL, microURL, testUser } from './Constants.js';
 
 export const options = {
   stages: [
     { duration: '1m', target: 10 },
-//    { duration: '1m', target: 20 },
-//    { duration: '1m', target: 0 },
+    { duration: '1m', target: 20 },
+    { duration: '1m', target: 20 },
+    { duration: '1m', target: 10 },
+    { duration: '1m', target: 0 },
   ],
 //  thresholds: { http_req_duration: ['avg<100', 'p(95)<200'] },
   noConnectionReuse: true,
   userAgent: 'MyK6UserAgentString/1.0',
 };
 
-export default function () {
+export default async function () {
   const url =
-    "http://k1s.fillerserver.net:30001/api/File/00000000-0000-0000-0000-000000000002/PDF?markAnswers=true";
+    `${microURL}/api/File/00000000-0000-0000-0000-000000000002/PDF?markAnswers=true`;
+  const TOKEN = await getTestUserAuthToken(microURL);
 
   const params = {
     headers: {
