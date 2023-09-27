12fb5cbf40d9032b94cfa0f964631f5bea3f3de9
Florian Feka <florianfeka05@gmail.com>
2023-09-27 21:01:39 +0200

    update load tests

--- api_performance_test/allExecTest.sh
+++ api_performance_test/allExecTest.sh
@@ -5,13 +5,13 @@ export K6_INFLUXDB_ORGANIZATION=examich
 export K6_INFLUXDB_TOKEN=lbvnJ4mE0c5XL3Au5mopYPiW7iY_YJS1j6YyntvmdIxF5v36kXRjkll2rVQglgbqthAx7DcC-nYaM6y5Exlshg==
 export K6_INFLUXDB_PUSH_INTERVAL=4s
 
-K6_INFLUXDB_BUCKET=examich_exam_micro ./k6 run --verbose -e MONO=false -o xk6-influxdb tests/Exams.js
-K6_INFLUXDB_BUCKET=examich_exam_mono ./k6 run --verbose -e MONO=true -o xk6-influxdb tests/Exams.js
-K6_INFLUXDB_BUCKET=examich_file_micro ./k6 run --verbose -e MONO=false -o xk6-influxdb tests/FileMicro.js
-K6_INFLUXDB_BUCKET=examich_file_mono ./k6 run --verbose -e MONO=true -o xk6-influxdb tests/FileMono.js
-K6_INFLUXDB_BUCKET=examich_login_micro ./k6 run --verbose -e MONO=false -o xk6-influxdb tests/Login.js
-K6_INFLUXDB_BUCKET=examich_login_mono ./k6 run --verbose -e MONO=true -o xk6-influxdb tests/Login.js
-K6_INFLUXDB_BUCKET=examich_question_micro ./k6 run --verbose -e MONO=false -o xk6-influxdb tests/Questions.js
-K6_INFLUXDB_BUCKET=examich_question_mono ./k6 run --verbose -e MONO=true -o xk6-influxdb tests/Questions.js
-K6_INFLUXDB_BUCKET=examich_user_micro ./k6 run --verbose -e MONO=false -o xk6-influxdb tests/Users.js
-K6_INFLUXDB_BUCKET=examich_user_mono ./k6 run --verbose -e MONO=true -o xk6-influxdb tests/Users.js
+K6_INFLUXDB_BUCKET=examich_exam_micro ./k6 run -e MONO=false -o xk6-influxdb tests/Exams.js
+K6_INFLUXDB_BUCKET=examich_exam_mono ./k6 run -e MONO=true -o xk6-influxdb tests/Exams.js
+K6_INFLUXDB_BUCKET=examich_file_micro ./k6 run -e MONO=false -o xk6-influxdb tests/FileMicro.js
+K6_INFLUXDB_BUCKET=examich_file_mono ./k6 run -e MONO=true -o xk6-influxdb tests/FileMono.js
+K6_INFLUXDB_BUCKET=examich_login_micro ./k6 run -e MONO=false -o xk6-influxdb tests/Login.js
+K6_INFLUXDB_BUCKET=examich_login_mono ./k6 run -e MONO=true -o xk6-influxdb tests/Login.js
+K6_INFLUXDB_BUCKET=examich_question_micro ./k6 run -e MONO=false -o xk6-influxdb tests/Questions.js
+K6_INFLUXDB_BUCKET=examich_question_mono ./k6 run -e MONO=true -o xk6-influxdb tests/Questions.js
+K6_INFLUXDB_BUCKET=examich_user_micro ./k6 run -e MONO=false -o xk6-influxdb tests/Users.js
+K6_INFLUXDB_BUCKET=examich_user_mono ./k6 run -e MONO=true -o xk6-influxdb tests/Users.js
--- api_performance_test/tests/Exams.js
+++ api_performance_test/tests/Exams.js
@@ -2,22 +2,14 @@ import http from "k6/http";
 import { protocol, monoURL, microURL } from '/home/feka/Documents/projects/FH_Project_Examich/api_performance_test/pre/Constants.js';
 
 export const options = {
-  scenarios: {
-    // define scenarios
-    breaking: {
-      executor: "ramping-vus",
-      stages: [
-        { duration: '1m', target: 300 },
-        { duration: '1m', target: 300 },
-        { duration: '2m', target: 900 },
-        { duration: '4m', target: 900 },
-        { duration: '2m', target: 300 },
-        { duration: '1m', target: 300 },
-        { duration: '1m', target: 0 },
-        //....
-      ],
-    },
-  },
+  stages: [
+    { duration: '1m', target: 300 },
+    { duration: '1m', target: 300 },
+    { duration: '2m', target: 900 },
+    { duration: '2m', target: 900 },
+    { duration: '2m', target: 300 },
+    { duration: '1m', target: 300 },
+  ],
 //  thresholds: { http_req_duration: ['avg<100', 'p(95)<200'] },
   noConnectionReuse: true,
   userAgent: 'MyK6UserAgentString/1.0',
